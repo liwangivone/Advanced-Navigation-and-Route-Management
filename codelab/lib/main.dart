6a831fb05c6c83c14995c9b1a503a747dae8902f
liwangivone <liwangivone@gmail.com>
2024-12-08 00:49:50 +0800

    .

---
+++ codelab/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import 'views/pages/pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fleurish',
+      routes: {
+        '/': (context) => const OnboardingPage(),
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+        '/detail': (context) => const DetailPage(
+            image: 'assets/bouquet1.jpg',
+            title: 'blush bloom bouquet',
+            description: 'A delicate bouquet of pink blooms, perfect for celebrating love and life’s sweetest moments',
+        )
+      },
+      initialRoute: '/', // Halaman awal adalah onboarding
+      );
+    
+    
+  }
+
+}
---
+++ codelab/lib/views/pages/pages.dart
@@ -0,0 +1,12 @@
+library pages;
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:lottie/lottie.dart';
+
+
+part 'onboarding_page.dart';
+part 'login_page.dart';
+part 'homepage.dart';
+part 'detail_page.dart';
